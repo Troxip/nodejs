f3310fb9f3eee436809e00b18817c0d38673931a
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-17 19:28:25 +0200

    Node.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const { myAdd, mySubtract } = require("./utils.js");
+console.log("Hello World");
+
+console.log(myAdd(3, 35));
+console.log(mySubtract(33, 5));
---
+++ utils.js
@@ -0,0 +1,18 @@
+// const firstName = "Alice";
+
+// const greet = (name) => {
+//   return `Hello ${name}`;
+// };
+// module.exports = greet;
+
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  myAdd: add,
+  mySubtract: subtract,
+};
