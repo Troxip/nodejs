aa12cfcd9221816c1290826045fda84bb7c93a07
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-18 19:29:23 +0200

    Nodejs

--- FS-API/app.js
+++ FS-API/app.js
@@ -1,14 +1,48 @@
+const { error } = require("console");
 const fs = require("fs");
 
 // const dataBuffer = fs.readFileSync("./sample.txt");
 // const content = dataBuffer.toString();
 // console.log(content);
 
-fs.readFile("./sample.txt", "utf-8", (err, content) => {
-  if (err) {
-    console.log(err);
-    throw err;
-  } else {
-    console.log(content);
-  }
-});
+// fs.readFile("./sample.txt", "utf-8", (err, content) => {
+//   if (err) {
+//     console.log(err);
+//     throw err;
+//   } else {
+//     console.log(content);
+//   }
+// });
+
+// fs.writeFile("./new.txt", "some new contentffff", (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   } else {
+//     console.log("File has been written");
+//   }
+// });
+
+// fs.appendFile("./new.txt", " Appended content", (error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("File content appended");
+//   }
+// });
+
+// fs.access("./new.txt", fs.constants.F_OK, (error) => {
+//   if (error) {
+//     console.log("File does not exist");
+//   } else {
+//     console.log("File exists");
+//   }
+// });
+
+// fs.unlink("./new.txt", (err) => {
+//   if (err) {
+//     console.log("Error");
+//   } else {
+//     console.log("File deleted");
+//   }
+// });
