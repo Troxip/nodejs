a8f2b78837272d51a952089d93780e9f6c16c198
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-19 20:37:41 +0200

    push

---
+++ 1. OS-API/app.js
@@ -0,0 +1,14 @@
+const os = require("os");
+// console.log(os);
+
+// console.log("CPU Arch", os.arch());
+
+// console.log("CPU info", os.cpus());
+
+// console.log("NDNS", os.endianness());
+
+// console.log("Free memory", os.freemem());
+
+// console.log(os.homedir());
+
+console.log(os.hostname());
--- app.js
+++
@@ -1,5 +0,0 @@
-const { myAdd, mySubtract } = require("./utils.js");
-console.log("Hello World");
-
-console.log(myAdd(3, 35));
-console.log(mySubtract(33, 5));
