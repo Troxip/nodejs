2c3bee2460a99c1769454f1a6a93591558ece532
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-20 18:58:01 +0200

    gg

--- 3. NodeJs Server/server.js
+++ 3. NodeJs Server/server.js
@@ -14,3 +14,20 @@ const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`The server is running on http://localhost:${PORT}`);
 });
+
+const http = require("http");
+// console.log(http);
+
+const requestHandler = (req, res) => {
+  console.log(res);
+
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("Hello world");
+};
+
+const server = http.createServer(requestHandler);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`The server is running on http://localhost:${PORT}`);
+});
