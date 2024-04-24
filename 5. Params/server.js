fc5239b81ddc1e637edd4f92c105708d75c42355
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-24 21:56:11 +0200

    fix

--- 5. Params/server.js
+++ 5. Params/server.js
@@ -13,28 +13,3 @@ server.listen(PORT, () => {
 });
 const http = require("http");
 const url = require("url");
-
-const requestHandler = (req, res) => {
-  const passedUrl = url.parse(req.url, true);
-};
-
-const server = http.createServer(requestHandler);
-
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`The server is running on http://localhost:${PORT}`);
-});
-
-const http = require("http");
-const url = require("url");
-
-const requestHandler = (req, res) => {
-  const passedUrl = url.parse(req.url, true);
-};
-
-const server = http.createServer(requestHandler);
-
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`The server is running on http://localhost:${PORT}`);
-});
