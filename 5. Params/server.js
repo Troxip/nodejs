b566cdc2b2dd3ac3f4f1232b2495942b6ce776c9
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-25 19:02:28 +0200

    what

--- 5. Params/server.js
+++ 5. Params/server.js
@@ -3,6 +3,10 @@ const url = require("url");
 
 const requestHandler = (req, res) => {
   const passedUrl = url.parse(req.url, true);
+  const pathname = passedUrl.pathname;
+
+  const pathComponent = pathname.split("/").filter(Boolean);
+  console.log(pathComponent);
 };
 
 const server = http.createServer(requestHandler);
@@ -11,5 +15,3 @@ const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`The server is running on http://localhost:${PORT}`);
 });
-const http = require("http");
-const url = require("url");
